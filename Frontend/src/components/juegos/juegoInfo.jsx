2e84b45af363cede3b4f55b7e8069ead7cb18ffa
FJaviRodriguez <fjrodriguezruiz1708@gmail.com>
2025-06-05 22:00:00 +0200

    hbhjkbh

--- Frontend/src/components/juegos/juegoInfo.jsx
+++ Frontend/src/components/juegos/juegoInfo.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { eliminarJuego } from '../../services/adminService';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 import { toast } from 'react-hot-toast';
 
 const JuegoInfo = ({ juego, onDelete }) => {
