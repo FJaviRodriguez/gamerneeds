a58af34c9f96ca93eaac09e82b4039446e0bce3f
FJaviRodriguez <fjrodriguezruiz1708@gmail.com>
2025-06-05 22:05:52 +0200

    b bn

--- Frontend/src/components/juegos/juegoInfo.jsx
+++ Frontend/src/components/juegos/juegoInfo.jsx
@@ -66,7 +66,7 @@ const JuegoInfo = ({ juego, onDelete }) => {
             {isAdmin && (
               <button
                 onClick={handleDelete}
-                className="bg-red-600 text-white px-3 py-1 rounded-md hover:bg-red-700 transition-colors"
+                className="!bg-red-600 text-white px-3 py-1 rounded-md hover:!bg-red-700 transition-colors"
               >
                 Eliminar
               </button>
