3bf39cfc8ae9839cccdf9f4b2729f9078cb5816e
FJaviRodriguez <fjrodriguezruiz1708@gmail.com>
2025-06-06 17:47:22 +0200

    nlj

--- Backend/src/config/mailjet.js
+++ Backend/src/config/mailjet.js
@@ -1,8 +1,8 @@
-import { Mailjet } from 'node-mailjet';
+import Mailjet from 'node-mailjet';
 
-const mailjet = new Mailjet({
-  apiKey: process.env.MAILJET_PUBLIC_KEY,
-  apiSecret: process.env.MAILJET_SECRET_KEY
-});
+const mailjet = Mailjet.apiConnect(
+  process.env.MAILJET_PUBLIC_KEY,
+  process.env.MAILJET_SECRET_KEY
+);
 
 export default mailjet;
